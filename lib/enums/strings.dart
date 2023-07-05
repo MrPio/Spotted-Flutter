e30fff7dbbffa0e3fe54254241fd9ef0d6d01e84
SbattellaMattia <tiasba01@gmail.com>
2023-07-05 13:07:41 +0200

    first fragment(1)

---
+++ lib/enums/strings.dart
@@ -0,0 +1,5 @@
+const String appTitle = 'Spotted!';
+const String introTitle = 'Dai un nome\nai volti che incontri';
+const String introSubTitle = 'Pubblica annunci e trova\nle persone che stai cercando!';
+const String start = 'INIZIAMO';
+
--- lib/view/first_page.dart
+++ lib/view/first_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:spotted_flutter/view/fragments/first_fragment.dart';
 
 class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -9,53 +10,58 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
-  @override
-  Widget build(BuildContext context) {
-    const _PATH = "assets/images/";
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("${_PATH}first_access.jpg"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0xFF000000), Color(0x00000000)],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                margin: EdgeInsets.all(16.0),
-                child: const Padding(
-                  padding: EdgeInsets.only(top: 56.0),
-                  child: Text(
-                    'Spotted!',
-                    style: TextStyle(
-                      fontFamily: 'clicker_script_regular',
-                      fontSize: 52,
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+   int _currentPageIndex = 0;
+  final List<Widget> _pages = const [
+    FirstFragment(),
+  ];
+   @override
+   Widget build(BuildContext context) {
+     const _PATH = "assets/images/";
+     return MaterialApp(
+       home: Scaffold(
+         body: Stack(
+           fit: StackFit.expand,
+           children: [
+             Container(
+               decoration: const BoxDecoration(
+                 image: DecorationImage(
+                   image: AssetImage("${_PATH}first_access.jpg"),
+                   fit: BoxFit.cover,
+                 ),
+               ),
+             ),
+             Container(
+               decoration: const BoxDecoration(
+                 gradient: LinearGradient(
+                   colors: [Color(0xFF000000), Color(0x00000000)],
+                   begin: Alignment.bottomCenter,
+                   end: Alignment.topCenter,
+                 ),
+               ),
+             ),
+             Align(
+               alignment: Alignment.topCenter,
+               child: Container(
+                 margin: EdgeInsets.all(16.0),
+                 child: const Padding(
+                   padding: EdgeInsets.only(top: 56.0),
+                   child: Text(
+                     'Spotted!',
+                     style: TextStyle(
+                       fontFamily: 'clicker_script_regular',
+                       fontSize: 52,
+                       color: Colors.white,
+                     ),
+                     textAlign: TextAlign.center,
+                   ),
+                 ),
+               ),
+             ),
+             _pages[_currentPageIndex],
+           ],
+         ),
+       ),
+     );
   }
 
   @override
