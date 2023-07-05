b57799b454bf1a8214b1dc9ca447d1fefae78ab7
SbattellaMattia <tiasba01@gmail.com>
2023-07-05 19:32:42 +0200

    app logo e login fragment

--- lib/view/fragments/login_fragment.dart
+++ lib/view/fragments/login_fragment.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../enums/palette.dart';
+
 class LoginFragment extends StatefulWidget {
   const LoginFragment({super.key});
 
@@ -10,6 +12,81 @@ class LoginFragment extends StatefulWidget {
 class _LoginFragmentState extends State<LoginFragment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        color: Colors.transparent,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(30, 250, 30, 0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(26),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Text(
+                      'Email:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Password:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    const Text(
+                      '',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Palette.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(999),
+                        ),
+                      ),
+                    ),
+                    Divider(),
+
+                    SizedBox(height: 16),
+                    const Text(
+                      'Non sei ancora registrato?',
+                      textAlign: TextAlign.center,
+                    ),
+                    const Text(
+                      'Registrati',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
   }
 }
